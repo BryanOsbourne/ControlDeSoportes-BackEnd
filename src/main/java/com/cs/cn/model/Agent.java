83b66540e59f88c3bb0015fffd1558c81eaf8c65
Bryan Pacheco <97611868+BryanOsbourne@users.noreply.github.com>
2023-09-09 21:01:41 -0500

    Actualización Authorities
    Se actualiza Permisos de usuario admin y se protege endPoint para crear nuevos usuarios

--- src/main/java/com/cs/cn/model/Agent.java
+++ src/main/java/com/cs/cn/model/Agent.java
@@ -46,9 +46,8 @@ public class Agent extends Person implements UserDetails {
     private Role role;
 
     @Override
-    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     @Override
